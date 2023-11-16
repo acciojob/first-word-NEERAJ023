a91f9425eb0d0165d85bb8fde0d62f68e5f7c628
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-16 20:44:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	s=s.split(" ");
+    console.log(s);
+	for(let i=0; i<s.length; i++){
+		if(s[i]!=""){
+			return s[i];
+		}
+	}
+	return "";
 }
 
 // Do not change the code below
-
-const s = prompt("Enter String:");
+ const s = prompt("Enter String:");
 alert(firstWord(s));
